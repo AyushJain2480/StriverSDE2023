193c12bad91e86f4184fb887848d7afb1be54720
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-09 20:51:29 +0530

    Create MergeSortedArrays.java

---
+++ ArraysPart2/MergeSortedArrays.java
@@ -0,0 +1,41 @@
+
+// Using extra space 
+
+import java.io.*;
+import java.util.* ;
+
+public class Solution {
+    public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
+        int[] res = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < m && j < n){
+            if(arr1[i] <= arr2[j]){
+                res[k] = arr1[i];
+                i++;
+                k++;
+            }else if(arr1[i] > arr2[j]){
+                res[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i < m){
+           res[k] = arr1[i];
+           k++;
+           i++;
+        }
+
+        while(j < n){
+           res[k] = arr2[j];
+           j++;
+           k++;
+        }
+        return res;
+    }
+}
+
+
+// without using extra space
