e87dfb874f4e6197ee9d54ec6aa931174a35d01d
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-09 21:15:59 +0530

    Update MergeSortedArrays.java

--- ArraysPart2/MergeSortedArrays.java
+++ ArraysPart2/MergeSortedArrays.java
@@ -39,3 +39,19 @@ public class Solution {
 
 
 // without using extra space
+
+
+public class Solution {
+    public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m + n) - 1;
+        while(i >= 0 && j >= 0){
+            arr1[k--] = (arr2[j] > arr1[i]) ? arr2[j--] : arr1[i--];
+        }
+        while(j >= 0){
+           arr1[k--] = arr2[j--];
+        }
+        return arr1;
+    }
+}
