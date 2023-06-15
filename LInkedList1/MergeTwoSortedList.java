df0971abc56bb615442712868cfb92c2cd7015fb
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-15 13:53:00 +0530

    Update MergeTwoSortedList.java

--- LInkedList1/MergeTwoSortedList.java
+++ LInkedList1/MergeTwoSortedList.java
@@ -1,3 +1,4 @@
+// better approach but honestly not that intuitive 
 public class Solution {
 	public static LinkedListNode<Integer> sortTwoLists(LinkedListNode<Integer> list1, LinkedListNode<Integer> list2) {
 		LinkedListNode dummy = new LinkedListNode(0);
@@ -24,3 +25,31 @@ public class Solution {
         return dummy.next;
 	}
 }
+
+
+// using space and sorting 
+
+ public ListNode mergeTwoLists(ListNode list1 , ListNode list2){
+        ArrayList<Integer> list = new ArrayList<>();
+        while(list1 != null){
+            list.add(list1.val);
+            list1 = list1.next;
+        }
+
+        while(list2 != null){
+            list.add(list2.val);
+            list2 = list2.next;
+        }
+
+        Collections.sort(list);
+
+       ListNode dummy = new ListNode(0);
+       ListNode temp = dummy;
+       for(Integer i : list){
+           ListNode newNode = new ListNode();
+           newNode.val = i;
+           temp.next = newNode;
+           temp = temp.next;
+       }
+       return dummy.next;
+    }
