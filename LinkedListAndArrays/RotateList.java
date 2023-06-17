6b0cbf2f3b05eaa227211cd8b1fd58ee4de74e69
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-17 13:19:21 +0530

    Update RotateList.java

--- LinkedListAndArrays/RotateList.java
+++ LinkedListAndArrays/RotateList.java
@@ -35,4 +35,38 @@ public class Solution {
 
 // Optimised solution 
 
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// O(N) + O(N - (N % K)) ~ O(N)
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0){
+            return head;
+        }
+        int count = 1;
+        ListNode tail = head;
+        
+        while(tail.next != null){
+            count++;
+            tail = tail.next;
+        }
+        
+        k = k % count;
+        
+        tail.next = head;
+        for(int i = 0 ; i < count - k; i++){
+            tail = tail.next;
+        }
+        head = tail.next;
+        tail.next = null;
+        return head;    
+    }
+}
