7aa0a96ca6512c242816e086367e26eaaf71fb5b
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-19 19:19:30 +0530

    Update MeetingRooms.java

--- Greedy/MeetingRooms.java
+++ Greedy/MeetingRooms.java
@@ -45,4 +45,30 @@ public class Main{
 
 
 // 2nd variation
+// Question is Find the minimum number of conference rooms required so that all meetings can be done.
+
+public class Solution {
+    public int solve(int[][] intervals) {
+    // sort on the basis of start
+    Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+    // storing end time in min heap
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    
+    for(int[] interval : intervals){
+        if(pq.size() == 0){
+            pq.add(interval[1]);
+        }
+        else{
+            if(interval[0] >= pq.peek()){
+            pq.remove();
+            pq.add(interval[1]);// old room has been occupied
+            }else{
+                pq.add(interval[1]); // creating a new room
+            }   
+        }
+    }
+    return pq.size(); // denotes the no. of rooms required
+    }
+}
+
 
