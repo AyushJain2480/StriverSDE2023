45bb767d8cd8fcc06068ec9c9ca3bad56928b597
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-22 23:31:18 +0530

    Update combinationSUm2.java

--- Recursion/combinationSUm2.java
+++ Recursion/combinationSUm2.java
@@ -1,3 +1,36 @@
+// Leetcode 
+// COmbination sum I- we can select one element multiple times
+class Solution {
+    
+    public void f(int ind, int[] arr, int target,List<List<Integer>> ans ,List<Integer> ds){
+        if(ind == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds)); // deep copy
+            }
+            return;
+        }
+        // pick
+        if(arr[ind] <= target){
+        // add element into ds and pass into the function    
+        ds.add(arr[ind]);
+        // ind again because we can select the same element again    
+        f(ind, arr, target - arr[ind], ans , ds);
+        // backtracking so remove from the ds     
+        ds.remove(ds.size() - 1);
+        }
+        // not pick
+        f(ind + 1, arr, target , ans , ds); 
+    }
+    
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        f(0,arr,target, res, new ArrayList<>());
+        return res;
+    }
+}
+
+
+
 import java.util.*;
 
 public class Solution {
