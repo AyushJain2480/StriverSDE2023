5a70818219f3000e4d0a31e7da38aeaca4501a25
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-06-27 16:48:01 +0530

    Update SingleElement.java

--- BinarySearch/SingleElement.java
+++ BinarySearch/SingleElement.java
@@ -43,3 +43,41 @@ public class Solution
 
 
 return arr.stream().reduce(0, (a,b) -> (a^b));
+
+
+
+
+// Better and optimised approach using binary search 
+
+ public static int singleNonDuplicate(ArrayList<Integer> arr)
+    { 
+        int n = arr.size();
+        if(n == 1) return arr.get(0);
+        if(arr.get(0) != arr.get(1)) return arr.get(0);
+        if(arr.get(n - 1) != arr.get(n - 2)) return arr.get(n - 1);
+
+
+        int lo = 1;
+        int hi = n - 2;
+
+        while(lo <= hi){
+           int mid = (lo + hi) / 2;
+           if(arr.get(mid) != arr.get(mid - 1) && arr.get(mid) != arr.get(mid + 1)){
+               return arr.get(mid);
+           }
+           if(mid % 2 == 0){
+                if(arr.get(mid) == arr.get(mid + 1)){
+                    hi = mid - 1;
+                }else if(arr.get(mid) == arr.get(mid - 1)){
+                    lo = mid + 1;
+                }
+           }else{
+               if(arr.get(mid) == arr.get(mid - 1)){
+                   lo = mid + 1;
+               }else if(arr.get(mid) == arr.get(mid + 1)){
+                   hi = mid - 1;
+               }
+           }
+        }
+        return -1;
+    }
