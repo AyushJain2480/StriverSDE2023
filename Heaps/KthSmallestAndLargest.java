e0eabec690f9cd9feebbd6ee71ca75abb5fc7eca
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-07-01 12:24:13 +0530

    Update KthSmallestAndLargest.java

--- Heaps/KthSmallestAndLargest.java
+++ Heaps/KthSmallestAndLargest.java
@@ -10,3 +10,39 @@ public class Solution {
 		return list;
 	}
 }
+//  If k is significantly smaller than n, the min heap approach is generally more efficient. However, if k is close to n or comparable to n,
+// the sorting approach may be more appropriate.
+import java.util.*;
+public class Solution {
+	public static ArrayList<Integer> kthSmallLarge(ArrayList<Integer> arr, int n, int k) {
+		// overall complexity -> O(nlogk)
+		ArrayList<Integer> list = new ArrayList<>();
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		for(Integer val : arr){
+			minHeap.add(val); // take O(n)
+		}
+		int kthSmall = 0 , kthLarge = 0;
+		int s = 1, l = n;
+
+		while(!minHeap.isEmpty()){
+			int pol = minHeap.poll(); // take O(logk)
+			if(s == k || l == k){
+				if(s == k){
+					kthSmall = pol;
+				}
+				if(l == k){
+					kthLarge = pol;
+				}
+				if(s > k && l < k){
+					break;
+				}
+			}
+			s++;
+			l--;
+		}
+		list.add(kthSmall);
+		list.add(kthLarge);
+        return list;
+	}
+}
+
