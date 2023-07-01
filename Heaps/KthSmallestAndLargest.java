e421a05e402afc383d6ed74f3efb0aafae286aa2
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-07-01 13:13:38 +0530

    Update KthSmallestAndLargest.java

--- Heaps/KthSmallestAndLargest.java
+++ Heaps/KthSmallestAndLargest.java
@@ -46,3 +46,17 @@ public class Solution {
 	}
 }
 
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int num: nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.remove();
+            }
+        }
+        
+        return heap.peek();
+    }
+}
+
