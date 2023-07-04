6db1072775fe326f886a9128971084ebfa97e29f
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-07-04 23:37:03 +0530

    Create LCS.java

---
+++ DP/LCS.java
@@ -0,0 +1,18 @@
+public class Solution {
+	public static int lcs(String s, String t) {
+    int m = s.length();
+    int n = t.length();
+		int[][] dp = new int[m + 1][n + 1];
+        
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(s.charAt(i) == t.charAt(j)){
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
