d29dd50e948566e6721a37394b5ba7946e0e169d
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-07-21 14:28:28 +0530

    Update LIS.java

--- DP/LIS.java
+++ DP/LIS.java
@@ -1,3 +1,4 @@
+// LIS
 import java.util.*;
 public class Solution {
 	public static int longestIncreasingSubsequence(int arr[]) {
@@ -32,4 +33,42 @@ class Solution {
        }
        return maxList;
    }
+}
+
+// Find Number of LIS
+
+class Solution {
+    public int findNumberOfLIS(int[] arr) {
+      int n = arr.length;
+      int[] dp = new int[n];
+      int[] count = new int[n];
+      
+      Arrays.fill(dp,1);
+      Arrays.fill(count,1);
+      int maxi = 1;
+        
+      for(int i = 1; i < n; i++){
+          for(int j = 0; j <= i - 1; j++){
+              if(arr[i] > arr[j]){
+                  // if you find the greater lis 
+                  if(dp[i] < dp[j] + 1){
+                      dp[i] = dp[j] + 1;
+                      count[i] = count[j];
+                  }
+                  // if it is the same of length inc the count only
+                  else if(dp[i] == dp[j] + 1){
+                      count[i] += count[j];
+                  }
+              }
+          }
+          maxi = Math.max(maxi, dp[i]); // lis maxm length
+      } 
+      int nos = 0;
+      for(int i = 0; i < n; i++){
+          if(dp[i] == maxi){
+              nos += count[i];
+          }
+      }  
+       return nos; 
+    }
 }
