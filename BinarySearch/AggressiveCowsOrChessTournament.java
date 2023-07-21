d68e894ccdf8d2537d7479e9fe0cfa6c45398807
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-07-21 09:53:18 +0530

    Update AggressiveCowsOrChessTournament.java

--- BinarySearch/AggressiveCowsOrChessTournament.java
+++ BinarySearch/AggressiveCowsOrChessTournament.java
@@ -1,3 +1,41 @@
+// O(max - min) * O(n) ---> quadritic time complexity
+// O(1) 
+import java.util.*;
+public class Solution {
+    public static int aggressiveCows(int[] A, int cows) {
+       Arrays.sort(A);
+       int min = A[0];
+       int max = A[A.length - 1];
+       // checking min dist possible
+       // max - min is possible max ans means maxm distance possible b/w 2 cows
+       int i = 1;
+       for(;i <= max - min; i++){
+           if(canWePlace(A,cows,i)){
+               continue;
+           }
+           return i - 1; // maxm min distance b/w cows
+       }
+       return i - 1;
+    }
+
+    public static boolean canWePlace(int[] A, int cows , int dist){
+        // first cow must be at 0th pos 
+        int cowsPlaced = 1, lastCow = A[0];
+
+        // placing other cows
+        for(int i = 1; i < A.length; i++){
+            if(A[i] - lastCow >= dist){
+                cowsPlaced++;
+                lastCow = A[i];
+            }
+            if(cowsPlaced >= cows) return true;
+        }
+        return false;
+    }
+}
+
+// optimised..
+
 import java.util.* ;
 import java.io.*; 
 public class Solution {
